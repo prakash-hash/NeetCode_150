d126d357ff7def0b406e779345051472a474b3ae
Prakash <pk46833@gmail.com>
2026-05-19 17:34:21 +0530

    Add: three-integer-sum - submission-7

---
+++ Data Structures & Algorithms/three-integer-sum/submission-7.cs
@@ -0,0 +1,42 @@
+public class Solution {
+    public List<List<int>> ThreeSum(int[] nums) {
+        List<List<int>> result = new List<List<int>>();
+        Array.Sort(nums);
+        for(int i = 0; i < nums.Length-2; i++){
+            if(nums[i] > 0){
+                // all numbers are positive so no further solution will be possible
+                break;
+            }
+            if(i > 0 && nums[i] == nums[i-1]){
+                continue;
+            }
+            int target = -nums[i];
+            int j = i+1;
+            int k = nums.Length-1;
+            while(j < k){
+                int sum = nums[j]+nums[k]; 
+                if(sum == target){
+                    result.Add(new List<int>(){nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+
+                    while(j < k && nums[j] == nums[j-1]){
+                        j++;
+                    }
+
+                    while(j < k && nums[k] == nums[k+1]){
+                        k--;
+                    }
+                }
+                else if(sum < target){
+                    j++;
+                }
+                else{
+                    k--;
+                }
+            }
+        }
+        return result;
+
+    }
+}
