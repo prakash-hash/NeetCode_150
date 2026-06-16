145987217ec9e3254ac3b1657677d62f216cf8cc
Prakash <pk46833@gmail.com>
2026-06-16 13:54:46 +0530

    Add: add-two-numbers - submission-0

---
+++ Data Structures & Algorithms/add-two-numbers/submission-0.cs
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
+        int carry = -1;
+        ListNode currH1 = l1;
+        ListNode currH2 = l2;
+        ListNode newHead = new ListNode(0);
+        ListNode currNewH = newHead;
+
+        while(currH1 != null && currH2 != null){
+            int sum = currH1.val + currH2.val;
+            sum = carry == -1 ? sum : sum + carry;
+            carry = sum > 9 ? 1 : -1;
+            currNewH.next = new ListNode(sum%10);
+            currNewH = currNewH.next;
+            currH1 = currH1.next;
+            currH2 = currH2.next;
+        }
+
+        while(currH1 != null){
+            int sum = carry  == -1 ? currH1.val : currH1.val + carry;
+            carry = sum > 9 ? 1 : -1;
+            currNewH.next = new ListNode(sum%10);
+            currNewH = currNewH.next;
+            currH1 = currH1.next;
+        }
+
+        while(currH2 != null){
+            int sum = carry  == -1 ? currH2.val : currH2.val + carry;
+            carry = sum > 9 ? 1 : -1;
+            currNewH.next = new ListNode(sum%10);
+            currNewH = currNewH.next;
+            currH2 = currH2.next;
+        }
+
+        if(carry == 1){
+            currNewH.next = new ListNode(1);
+        }
+
+        return newHead.next;
+    }
+}
