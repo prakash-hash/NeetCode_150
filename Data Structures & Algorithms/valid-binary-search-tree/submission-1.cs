705480117edde37371fad009a4033406dc401798
Prakash <pk46833@gmail.com>
2026-07-02 23:28:17 +0530

    Add: valid-binary-search-tree - submission-1

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-1.cs
@@ -0,0 +1,19 @@
+public class Solution
+{
+    public bool IsValidBST(TreeNode root)
+    {
+        return Validate(root, long.MinValue, long.MaxValue);
+    }
+
+    private bool Validate(TreeNode node, long min, long max)
+    {
+        if (node == null)
+            return true;
+
+        if (node.val <= min || node.val >= max)
+            return false;
+
+        return Validate(node.left, min, node.val) &&
+               Validate(node.right, node.val, max);
+    }
+}
