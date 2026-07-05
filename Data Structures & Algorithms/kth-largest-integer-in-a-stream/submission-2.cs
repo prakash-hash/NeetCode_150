b59b204bcba248e4f9ec2a79a80871936ac8994f
Prakash <pk46833@gmail.com>
2026-07-05 20:17:37 +0530

    Add: kth-largest-integer-in-a-stream - submission-2

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-2.cs
@@ -0,0 +1,30 @@
+
+
+public class KthLargest
+{
+    private readonly int k;
+        private readonly PriorityQueue<int, int> minHeap;
+
+            public KthLargest(int k, int[] nums)
+                {
+                        this.k = k;
+                                minHeap = new PriorityQueue<int, int>();
+
+                                        foreach (int num in nums)
+                                                {
+                                                            Add(num);
+                                                                    }
+                                                                        }
+
+                                                                            public int Add(int val)
+                                                                                {
+                                                                                        minHeap.Enqueue(val, val);
+
+                                                                                                if (minHeap.Count > k)
+                                                                                                        {
+                                                                                                                    minHeap.Dequeue();
+                                                                                                                            }
+
+                                                                                                                                    return minHeap.Peek();
+                                                                                                                                        }
+                                                                                                                                        }
