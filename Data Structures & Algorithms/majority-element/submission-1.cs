c0e271d9ff47366b23c84ed897a7544e72604585
Prakash <pk46833@gmail.com>
2026-07-27 02:03:47 +0530

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.cs
@@ -0,0 +1,23 @@
+// public class Solution {
+//     public int MajorityElement(int[] nums) {
+//         Array.Sort(nums);
+//         return nums[nums.Length/2];
+//     }
+// }
+
+public class Solution {
+    public int MajorityElement(int[] nums) {
+        int res = 0;
+        int count = 0;
+        
+        foreach(int n in nums){
+            if(count == 0){
+                res = n;
+            }
+
+            count += (n == res ? 1 : -1);
+        }
+
+        return res;
+    }
+}
