c920b63f92394cd556f6ab2a9ccf627f452c03f5
Prakash <pk46833@gmail.com>
2026-07-27 22:45:46 +0530

    Add: design-hashset - submission-1

---
+++ Data Structures & Algorithms/design-hashset/submission-1.cs
@@ -0,0 +1,27 @@
+public class MyHashSet {
+    bool[] hashSet;
+    
+    public MyHashSet() {
+        hashSet = new bool[1000001];
+    }
+    
+    public void Add(int key) {
+        hashSet[key] = true;
+    }
+    
+    public void Remove(int key) {
+        hashSet[key] = false;
+    }
+    
+    public bool Contains(int key) {
+        return hashSet[key];
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.Add(key);
+ * obj.Remove(key);
+ * bool param_3 = obj.Contains(key);
+ */
