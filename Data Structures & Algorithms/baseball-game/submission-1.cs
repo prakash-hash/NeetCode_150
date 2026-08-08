d098fa80a6f58932d8680d1b30a5412b42a70f38
Prakash <pk46833@gmail.com>
2026-08-09 00:58:13 +0530

    Add: baseball-game - submission-1

---
+++ Data Structures & Algorithms/baseball-game/submission-1.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int CalPoints(string[] operations) {
+        List<int> scores = new();
+        foreach(string op in operations){
+            int len = scores.Count();
+            if(op == "+"){
+                scores.Add(scores[len-1]+scores[len-2]);
+            }
+            else if(op == "C"){
+                scores.RemoveAt(len - 1);
+            }
+            else if(op == "D"){
+                scores.Add((scores[len-1])*2);
+            }
+            else{
+                scores.Add(int.Parse(op));
+            }
+        }
+
+        return scores.Sum();
+    }
+}
